3c423a543e971f1a6879c0151d1ba61a38c670aa
Tim Maupin <timmaupin@live.com>
2023-04-04 09:44:37 -0500

    starting let newPet

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict'
+console.log('dawgs baby!')
 
 
 
@@ -6,6 +7,7 @@ let petArray = [];
 
 
 function Pet(petName, imgSrc, type, breed, age, gender, kidFriendly, petFriendly){
+    console.log("🚀 ~ file: app.js:10 ~ Pet ~ Pet:", Pet)
     this.petName = petName
     this.type = type
     this.breed = breed
@@ -18,3 +20,4 @@ function Pet(petName, imgSrc, type, breed, age, gender, kidFriendly, petFriendly
 
 }
 
+let newPet = Pet()
