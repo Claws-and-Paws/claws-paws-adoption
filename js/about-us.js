1639ea3d340bcd62a7ba4eede6ef96aedf8da0e4
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-05 16:50:12 -0500

    fix image display of biographies, so when you click on image our bios appear

--- js/about-us.js
+++ js/about-us.js
@@ -1,16 +1,43 @@
 "use strict"
 console.log('About Us js working!');
 
+// hidden biography
+let bio = document.getElementById('first-bio');
+let bioTwo = document.getElementById('second-bio');
+let bioThree = document.getElementById('third-bio');
 
-let bio = document.getElementById('biographies');
-
-function appearBio(event) {
-    const bioSection = event.target('.images');
-    document.querySelector('images');
-    bio.addEventListener('click', showBio);
-    showBio();
+function appearBio() {
+    console.log('we made');
+    let x = document.getElementById('first-bio');
+    console.log(x.style)
+    if (x.style.visibility === "hidden") {
+        console.log('The bio!');
+        x.style.visibility = 'visible';
+    } else {
+        x.style.visibility = 'hidden';
+    }
 }
 
-function showBio() {
-    bio.removeAttribute('hidden');
-}
+// function appearBioTwo() {
+//     let y = document.getElementById('second-bio');
+//     if (y.style.visibility === "hidden") {
+//         y.style.visibility = 'visible';
+//     } else {
+//         y.style.visibility = 'hidden';
+//     }
+// }
+
+
+
+const imagesT = document.getElementById('tricia');
+console.log("🚀 ~ file: about-us.js:21 ~ images:", imagesT);
+
+
+// get Tim's image first and then it should work
+// const imagesTim = document.getElementById('tim');
+
+
+// const imagesBryan = document.getElementById('bryan');
+
+imagesT.addEventListener('click', appearBio)
+// imagesTim.addEventListener('click', appearBioTwo)
